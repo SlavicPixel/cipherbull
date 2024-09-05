5af98ce2d00b0bfa196dc1a6ccaf583bbe90e197
pixel <domi9132@gmail.com>
2024-09-05 17:22:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cipherbull
+
+Cross-platform password manager
---
+++ android/app/src/main/kotlin/com/example/cipherbull/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cipherbull
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
