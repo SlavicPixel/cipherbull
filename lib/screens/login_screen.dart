ea87b87e4e94603ada972d0793dfb6cd55c44cd0
pixel <domi9132@gmail.com>
2024-09-10 17:57:09 +0200

    Added a button to create new vault and checks for login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,15 @@
 // lib/screens/login_screen.dart
 import 'package:flutter/material.dart';
 import 'package:cipherbull/services/secure_storage_helper.dart';
+import 'package:cipherbull/services/database_helper.dart';
 import 'package:cipherbull/screens/home_screen.dart';
+import 'package:cipherbull/screens/register_screen.dart';
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _LoginScreenState createState() => _LoginScreenState();
 }
 
@@ -18,31 +23,50 @@ class _LoginScreenState extends State<LoginScreen> {
     String password = _passwordController.text;
 
     if (dbName.isNotEmpty && password.isNotEmpty) {
-      await _secureStorageService.storeDatabaseName(dbName);
-      await _secureStorageService.storeDatabasePassword(password);
+      final dbHelper = DatabaseHelper(dbName: dbName, dbPassword: password);
+      if (await dbHelper.checkDatabaseExists()) {
+        try {
+          await dbHelper.getEntries();
 
-      // Navigate to home screen with the dbName and dbPassword
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => HomeScreen(),
-        ),
-      );
+          await _secureStorageService.storeDatabaseName(dbName);
+          await _secureStorageService.storeDatabasePassword(password);
+
+          if (mounted) {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (context) => const HomeScreen(),
+              ),
+            );
+          }
+        } catch (e) {
+          _showErrorDialog("Incorrect password or database name.");
+        }
+      } else {
+        _showErrorDialog("Database with name '$dbName' does not exist.");
+      }
     } else {
-      // Show error
       _showErrorDialog('Please enter both username and password');
     }
+  }
+
+  void _createVault() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const RegisterScreen(),
+      ),
+    );
   }
 
   void _showErrorDialog(String message) {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: Text('Error'),
+        title: const Text('Error'),
         content: Text(message),
         actions: [
           TextButton(
             onPressed: () => Navigator.of(context).pop(),
-            child: Text('OK'),
+            child: const Text('OK'),
           ),
         ],
       ),
@@ -52,25 +76,36 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login/Register')),
+      appBar: AppBar(title: const Text('CipherBull')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const Text(
+              "Sign In",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
             TextField(
               controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: const InputDecoration(labelText: 'Username'),
             ),
             TextField(
               controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: const InputDecoration(labelText: 'Password'),
               obscureText: true,
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _login,
-              child: Text('Login/Register'),
+              child: const Text('Login'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30, bottom: 5),
+              child: ElevatedButton(
+                onPressed: _createVault,
+                child: const Text('Create new vault'),
+              ),
             ),
           ],
         ),
